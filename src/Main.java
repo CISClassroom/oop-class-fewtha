0714cdfeabc04e5a2bdce7eb0c56575dc8dc4e1c
Punjapon <punjapon@kkumail.com>
2019-12-27 23:22:59 +0700

    2OOP

--- src/Main.java
+++ src/Main.java
@@ -3,44 +3,26 @@ import com.company.Student;
 public class Main {
 
     public static void main(String[] args) {
-        // create Student object
-        Student mark = new Student();
-        Student tontan = new Student();
-        Student dream = new Student();
+        Student few = new Student();
 
-        //input data to Object
-        mark.name = "X Mark";
-        mark.id = "001";
-        mark.major = "CIS";
-        tontan.name = "Ton Tan";
-        tontan.id = "002";
-        tontan.major = "CIS";
-        dream.name = "Dream all night";
-        dream.id = "003";
-        dream.major = "CS";
+        few.name = "thanagon piromchai";
+        few.id = "623410034-7";
+        few.major = "CIS";
+        //add your code here
+        System.out.println("1. " + few.name + " "+ few.major);
+        Student[] oopStudents = {few,};
+        System.out.println("1. "+oopStudents[0].name);
 
-        //get data from Object
-        //System.out.println("1. " + mark.name + " " + mark.major);
-        //System.out.println("2. " + tontan.name + " " + tontan.major);
-
-        // add student to array
-        // array index
-        Student[] oopStudent = {mark, tontan};
-
-        System.out.println("1. " + oopStudent[0].name);
-        System.out.println("1. " + oopStudent[1].name);
+        find_CIS_Student(oopStudents);
 
     }
-    // To find CIS Student
+
     public static void find_CIS_Student(Student[] student){
         for(int i = 0; i < student.length; i++){
             if(student[i].major == "CIS"){
                 System.out.println(i+1 + ". " + student[i].name);
             }
         }
-    }
-    public void find_CS_Student(){
-
     }
 }
 
